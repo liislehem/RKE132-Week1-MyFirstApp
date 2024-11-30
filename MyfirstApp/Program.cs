a1c2c29f56bcdb5578d1e554f5cd22d4de5cbaf5
liis lehemets <liis.lehemets@tktk.ee>
2024-11-30 20:11:23 +0200

    create new app

---
+++ MyfirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajalt tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //output, käsk mida teha on alati sulgudes
+//string- sõne
+string userName= Console.ReadLine(); //Input
+
+Console.WriteLine("Hello"+ "," + userName + "!"); //output
+//string interpolation
+
+Console.WriteLine($"Hello {userName}  !"); //Output
